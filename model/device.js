75c6bdf9690f461c32ffea7d743cfaae04c8caf1
KienTran <coinskus001@gmail.com>
2024-10-06 18:36:48 +0700

    ok

--- model/device.js
+++ model/device.js
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose';
+import AutoIncrementFactory from 'mongoose-sequence';
+const connection = mongoose.createConnection('mongodb+srv://coinskus001:Dodaihoc9@cluster0.6clbaom.mongodb.net/Kien_Hoc_MERN?retryWrites=true&w=majority&appName=Cluster0');
+const AutoIncrement = AutoIncrementFactory(connection);
 
 const deviceShema = new mongoose.Schema({
+  id: { type: Number },
   tag: {
     type: String,
     required: true,
@@ -17,6 +21,7 @@ const deviceShema = new mongoose.Schema({
     default: "off"
   },
 }, { timestamps: true , collection: 'Device'});//chèn các collection vào bảng user1
+deviceShema.plugin(AutoIncrement, { inc_field: 'id' });
 // createdAt, updatedAt sẽ được tự động thêm vào bởi tùy chọn timestamps
 
 export const Device = mongoose.model('Device', deviceShema);
